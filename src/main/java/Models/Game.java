809abc0bc644bb96a461d01a7b08f6464c6e53d5
Daniels,Martijn M.J.S <martijn.daniels@student.fontys.nl>
2019-10-28 10:03:50 +0100

    fixes a missing initialization

--- src/main/java/Models/Game.java
+++ src/main/java/Models/Game.java
@@ -34,6 +34,7 @@ public class Game {
     private void setUpMap(){
         walls = new ArrayList<>();
         ghosts = new ArrayList<>();
+        items = new ArrayList<>();
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 switch (map[i][j]) {
