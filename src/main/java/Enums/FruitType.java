13956e01aed97026c0b4f94dc0ae4cc93b534add
Daniels,Martijn M.J.S <martijn.daniels@student.fontys.nl>
2019-11-04 09:40:28 +0100

    makes fruittype score a little bit better

--- src/main/java/Enums/FruitType.java
+++ src/main/java/Enums/FruitType.java
@@ -2,7 +2,25 @@ package Enums;
 
 public enum FruitType
 {
-    Apple,
-    Banana,
-    Cherry
+    Apple{
+        @Override
+        public int getScoreValue(){
+            return 100;
+        }
+    },
+    Banana{
+        @Override
+        public int getScoreValue(){
+            return 250;
+        }
+    },
+    Cherry{
+        @Override
+        public int getScoreValue(){
+            return 500;
+        }
+    };
+
+    public abstract int getScoreValue();
 }
+
